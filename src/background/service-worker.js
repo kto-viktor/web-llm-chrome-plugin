0cabbde7fe170b449170730ff0e3c07989ad7bdd
viktor <kto.viktor.kto@gmail.com>
2026-02-06 13:16:23 +0300

    open welcome page after install

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -30,6 +30,26 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    // Code to be executed on first install
+    // eg. open a tab with a url
+    chrome.tabs.create({
+      url: "https://staging.d1b4q74qfasigt.amplifyapp.com/",
+    });
+  } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+    // When extension is updated
+  } else if (
+      details.reason === chrome.runtime.OnInstalledReason.CHROME_UPDATE
+  ) {
+    // When browser is updated
+  } else if (
+      details.reason === chrome.runtime.OnInstalledReason.SHARED_MODULE_UPDATE
+  ) {
+    // When a shared module is updated
+  }
+});
+
 /**
  * Opens the side panel when the extension icon is clicked.
  */
